90a48e2ccf10e3436be4c9db09c4ca59339d8cd3
Yusuf Farhan <dalas98@gmail.com>
2021-07-01 10:27:23 +0700

    add report

--- admin/report/all_reports.php
+++ admin/report/all_reports.php
@@ -31,10 +31,107 @@ include('../connection.php');
         <div class="col-lg-12">
           <div class="card">
             <div class="card-body">
-              <h1 class="text-center">This page available soon.</h1>
+              <h5>Data Voucher</h5>
+              <table class="table table-bordered table-striped mt-3 mydatatable">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Code</th>
+                    <th>Description</th>
+                    <th>Quota</th>
+                    <th>Percent</th>
+                    <th>Start Date</th>
+                    <th>Expire Date</th>
+                    <th>Voucher Used</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $result = mysqli_query($link, "SELECT vouchers.*, COUNT(`order`.voucher_id) as voucher_used FROM vouchers LEFT JOIN `order` ON vouchers.id=`order`.voucher_id GROUP BY vouchers.id");
+                  while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                    <tr>
+                      <td><?=$row['id']?></td>
+                      <td><?=$row['code']?></td>
+                      <td><?=$row['description']?></td>
+                      <td><?=$row['quotas']?></td>
+                      <td><?=$row['percent']?>%</td>
+                      <td><?=$row['start_date']?></td>
+                      <td><?=$row['expired_date']?></td>
+                      <td><?=$row['voucher_used']?></td>
+                    </tr>
+                  <?php } ?>
+                </tbody>
+              </table>
             </div>
           </div>
+        </div>
 
+        <div class="col-lg-12">
+          <div class="card">
+            <div class="card-body">
+              <h5>Data Product</h5>
+              <table class="table table-bordered table-striped mt-3 mydatatable">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Stock</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                    $result = mysqli_query($link, "select * from product");
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                  ?>
+                    <tr>
+                      <td><?=$row['id']?></td>
+                      <td><?=$row['name']?></td>
+                      <td><?=$row['stock']?></td>
+                      <td>Rp. <?=number_format($row['price'])?></td>
+                    </tr>
+                  <?php } ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-12">
+          <div class="card">
+            <div class="card-body">
+              <h5>Data Order</h5>
+              <table class="table table-bordered table-striped mt-3 mydatatable">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Qty</th>
+                    <th>Total</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $result = mysqli_query($link, "SELECT u.name, o.* FROM `order` o INNER JOIN `user` u ON u.id=o.user_id");
+
+                  while ($row = mysqli_fetch_assoc($result)) {
+                  $class = $row['status'] == 'pending payment' ? 'warning' : ($row['status'] == 'proses' ? 'info' : ($row['status'] == 'dikirim' ? 'primary' : 'success'));
+                    ?>
+                    <tr>
+                      <td><?=$row['id']?></td>
+                      <td><?=$row['name']?></td>
+                      <td><?=$row['total_qty']?></td>
+                      <td>Rp. <?=number_format($row['total_price'])?></td>
+                      <td class="text-center"><h5><span class="badge badge-<?=$class?>"><?=ucwords($row['status'])?></span></h5></td>
+                    </tr>
+                  <?php } ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
 
       </div>
